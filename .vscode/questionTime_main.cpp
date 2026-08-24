9fa1ea404cece81c62a77465c7f83ba2018ece4c
oc0078 <emmaoconnor@my.unt.edu>
2026-08-24 12:42:22 -0500

    updated folder name and such

--- .vscode/questionTime_main.cpp
+++ .vscode/questionTime_main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <vector>
+#include "questionTime_questions.cpp"
 using namespace std;
 
 int main() {
     //This exists just to run the code tbh
     cout << "Hi I remember how to code" << endl;
+    Question test;
+
+    test.display();
+
     return 1;
 }
--- .vscode/questionTime_questions.cpp
+++ .vscode/questionTime_questions.cpp
@@ -1,12 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Question {
     public:
 
     void display() { //displays basic information
         cout << "Welcome to the Quiz questionaire! Do you wonder where you will be sorted out?" << endl;
-        cout << "type in a value 1-4 to choose your answer for each question, different values will reload the question" << endl;
+        cout << "\ntType in a value 1-4 to choose your answer for each question, different values will reload the question" << endl;
         cout << "you cannot leave the questionarie until ALL questions have been asked" << endl;
     }
 
     private:
         int score; //value that will determine what the player gets at the end
-}
+};
